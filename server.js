c89587a2375fae2bb1faf0d2a64c1a29d348595f
KngTeo <150524476+KngTeo@users.noreply.github.com>
2025-06-13 15:09:39 -0500

    Conexion crud Empleados,equipos y usuarios, se agrego la funcion de agregar,eliminar y editar.

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const usuariosRoutes = require('./routes/usuarios');
+const empleadosRoutes = require('./routes/empleados'); 
+const equiposRoutes = require('./routes/equipos');
+
+app.use('/api/usuarios', usuariosRoutes);
+app.use('/api/empleados', empleadosRoutes);
+app.use('/api/equipos', equiposRoutes);
+
+app.listen(3000, () => {
+  console.log('Servidor backend corriendo en http://localhost:3000');
+});
